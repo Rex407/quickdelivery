c98d36743c615372ad487ed6534aaf1181288ad0
Rex407 <rexkapolom@outlook.com>
2026-05-10 21:36:51 +0200

    landing page

---
+++ src/assets/components/pages/Home.jsx
@@ -0,0 +1,15 @@
+function Home() {
+  return (
+    <div className="p-10">
+      <h1 className="text-4xl font-bold">
+        Delivery App
+      </h1>
+
+      <p className="mt-4">
+        Food and parcel delivery platform
+      </p>
+    </div>
+  );
+}
+
+export default Home;
